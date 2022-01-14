151fb9e0f5965299103553c1dedd5ec5b5a26950
sylvester <nkwontasylvester15@gmail.com>
2022-01-14 10:45:39 -0900

    first commit today

--- use_cust/settings.py
+++ use_cust/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users_customer',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
--- users_customer/views.py
+++ users_customer/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
+            user = authenticate(username=username, password=raw_password)
+            Profile.objects.create(user=user, name=username, email=email)
+            #sending a success message
+            messages.success(request, f'Hello {username}, your account has been created! You are able to log in')
+            login(request, user)
+            return redirect('login')
+
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users_customer/register.html', {'form':form})
 
 def index(request):
     totals = Product.get_all_products()
@@ -23,10 +44,10 @@ def cart(request):
     #if the user is authenticated
     if request.user.is_authenticated:
         #setting the customer value. Simply setting the customer to profile that has the user.
-        profile = request.user.profile
+        consumer = request.user.consumer
         #creating an object or querying an object. Having setting the Order to the customer with complete status as false.
-        profile = profile.save()
-        order, created = Order.objects.get_or_create(profile=profile, complete=False)
+        consumer = consumer.save()
+        order, created = Order.objects.get_or_create(consumer=consumer, complete=False)
         #we getting items attached to the order. we getting all order items that has the order.
         items = order.orderitem_set.all()
 		#cartItems = order.get_cart_items
@@ -43,6 +64,7 @@ def cart(request):
 
 
 def checkout(request):
+
     context = {
 
     }
