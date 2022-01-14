9279f3784a721de9e9f97397fa8269d08bec9533
sylvester <nkwontasylvester15@gmail.com>
2022-01-13 19:00:19 -0900

    templates created

--- users_customer/views.py
+++ users_customer/views.py
@@ -6,11 +6,31 @@ from .models import *
 def index(request):
     totals = Product.get_all_products()
     tot_id = Customer.get_all_customers()
+    categories = Category.get_all_categories()
    
     context = {
 
         'totals': totals,
         'tot_id': tot_id,
+        'categories': categories,
     }
 
-    return render(request, 'users_customer/index.html', context )
+    return render(request, 'users_customer/index.html', context)
+
+def cartData(request):
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+		#cartItems = order.get_cart_items
+        # 
+    else:
+        items = []
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, 'users_customer/index.html', context)
+
