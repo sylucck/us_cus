6563eff2947e68e43b6d37da5a32d0ba286d146a
sylvester <nkwontasylvester15@gmail.com>
2022-01-15 12:23:24 -0900

    default.png added

--- e_commerce/forms.py
+++ e_commerce/forms.py
@@ -23,4 +23,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = [ 'image', 'address',  ]
+        fields = [ 'name', 'image', 'address', 'phone' ]
