94c96d979b3b29198e1e8354448b8be78b5a6133
sylvester <nkwontasylvester15@gmail.com>
2022-02-14 14:10:08 -0900

    commit

--- use_cust/settings.py
+++ use_cust/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -88,10 +88,10 @@ WSGI_APPLICATION = 'use_cust.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 #DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+    #'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': BASE_DIR / 'db.sqlite3',
+ #   }
 #}
 
 DATABASES = {
