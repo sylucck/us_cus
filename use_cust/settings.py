664b93e99245647609cde9e370db8ac457774290
sylvester <nkwontasylvester15@gmail.com>
2022-02-14 17:55:49 -0900

    more commit

--- use_cust/settings.py
+++ use_cust/settings.py
@@ -87,16 +87,10 @@ WSGI_APPLICATION = 'use_cust.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
-
 DATABASES = {
-    'default': dj_database_url.config()
+   'default': dj_database_url.config()
 }
+
 
 
 # Password validation
